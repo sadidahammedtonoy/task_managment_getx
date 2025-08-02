a27b5cf1906390813236610bfd9d50b7eb14ee7a
Sadid <saadid.2002@gmail.com>
2025-08-02 20:27:00 +0600

    task view

--- lib/Features/Task_managment/View/viewTask.dart
+++ lib/Features/Task_managment/View/viewTask.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:task_manager/Features/Task_managment/Model/model.dart';
 import 'package:task_manager/Features/Task_managment/Model/countModel.dart';
 import 'package:task_manager/Network/network.dart';
@@ -9,112 +10,24 @@ import '../../Global Widgets/snackbar.dart';
 import '../../Global Widgets/card.dart';
 import '../../Global Widgets/countCard.dart';
 import 'taskDetails.dart';
-
-class taskView extends StatefulWidget {
-  const taskView({super.key});
-
-  @override
-  State<taskView> createState() => _taskViewState();
-  static const String name = '/new-task-list';
-}
-
-class _taskViewState extends State<taskView> {
-  List<TaskModel> _newTaskList = [];
-  bool _isLoading = false;
-  bool _taskCountSummaryLoading = false;
-  List<TaskStatusCountModel> _taskCountSummaryList = [];
+final NetworkCaller controller = Get.put(NetworkCaller());
+class TaskViewController extends GetxController {
+  var newTaskList = <TaskModel>[].obs;
+  var isLoading = false.obs;
+  var taskCountSummaryLoading = false.obs;
+  var taskCountSummaryList = <TaskStatusCountModel>[].obs;
 
   @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _getNewTaskList();
-      _getTaskCountSummary();
-    });
+  void onInit() {
+    super.onInit();
+    getNewTaskList();
+    getTaskCountSummary();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            Visibility(
-              visible: _taskCountSummaryLoading == false,
-              replacement: CenteredCircularProgressIndicator(),
-              child: SizedBox(
-                height: 100,
-                child: ListView.separated(
-                  itemCount: _taskCountSummaryList.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return countcard(
-                      title: _taskCountSummaryList[index].sId!,
-                      count: _taskCountSummaryList[index].sum!,
-                    );
-                  },
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(width: 4),
-                ),
-              ),
-            ),
-            Visibility(
-              visible: _isLoading == false,
-              replacement: CenteredCircularProgressIndicator(),
-              child: Expanded(
-                child: ListView.builder(
-                  padding: EdgeInsets.only(bottom: 70),
-                  itemCount: _newTaskList.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => taskDetails(
-                              title: _newTaskList[index].title!,
-                              description: _newTaskList[index].description!,
-                              createdDate: formatDate(
-                                _newTaskList[index].createdDate!,
-                              ),
-                              status: _newTaskList[index].status!,
-                            ),
-                          ),
-                        );
-                      },
-                      child: card(
-                        taskType: TaskType.tNew,
-                        taskModel: _newTaskList[index],
-                        onTaskStatusUpdated: () {
-                          _getTaskCountSummary();
-                          _getNewTaskList();
-                        },
-                        onDeleteTask: () {
-                          _getTaskCountSummary();
-                          _getNewTaskList();
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  Future<void> getNewTaskList() async {
+    isLoading.value = true;
 
-  Future<void> _getNewTaskList() async {
-    _isLoading = true;
-    if (mounted) {
-      setState(() {});
-    }
-
-    NetworkResponse response = await networkCaller.getRequest(
+    NetworkResponse response = await controller.getRequest(
       url: urls.GetNewTasksUrl,
     );
     if (response.isSuccess) {
@@ -122,27 +35,18 @@ class _taskViewState extends State<taskView> {
       for (Map<String, dynamic> jsonData in response.body!['data']) {
         list.add(TaskModel.fromJson(jsonData));
       }
-      _newTaskList = list;
+      newTaskList.assignAll(list);
     } else {
-      if (mounted) {
-        showSnackBarMessage(context, response.errorMessage!);
-      }
+      showSnackBarMessage(Get.context!, response.errorMessage!);
     }
 
-    _isLoading = false;
-    if (mounted) {
-      setState(() {});
-    }
+    isLoading.value = false;
   }
 
-  Future<void> _getTaskCountSummary() async {
-    _taskCountSummaryLoading = true;
+  Future<void> getTaskCountSummary() async {
+    taskCountSummaryLoading.value = true;
 
-    if (mounted) {
-      setState(() {});
-    }
-
-    NetworkResponse response = await networkCaller.getRequest(
+    NetworkResponse response = await controller.getRequest(
       url: urls.GetAllTasksUrl,
     );
 
@@ -152,21 +56,90 @@ class _taskViewState extends State<taskView> {
         list.add(TaskStatusCountModel.fromJson(jsonData));
       }
       list.sort((a, b) => b.sum!.compareTo(a.sum!));
-      _taskCountSummaryList = list;
+      taskCountSummaryList.assignAll(list);
     } else {
-      if (mounted) {
-        showSnackBarMessage(context, response.errorMessage!);
-      }
+      showSnackBarMessage(Get.context!, response.errorMessage!);
     }
 
-    _taskCountSummaryLoading = false;
-    if (mounted) {
-      setState(() {});
-    }
+    taskCountSummaryLoading.value = false;
   }
+}
+
+class taskView extends StatelessWidget {
+  const taskView({super.key});
+
+  static const String name = '/new-task-list';
 
   @override
-  void dispose() {
-    super.dispose();
+  Widget build(BuildContext context) {
+    final controller = Get.put(TaskViewController());
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            Obx(() => Visibility(
+              visible: !controller.taskCountSummaryLoading.value,
+              replacement: CenteredCircularProgressIndicator(),
+              child: SizedBox(
+                height: 100,
+                child: Obx(() => ListView.separated(
+                  itemCount: controller.taskCountSummaryList.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return countcard(
+                      title: controller.taskCountSummaryList[index].sId!,
+                      count: controller.taskCountSummaryList[index].sum!,
+                    );
+                  },
+                  separatorBuilder: (context, index) =>
+                  const SizedBox(width: 4),
+                )),
+              ),
+            )),
+            Obx(() => Visibility(
+              visible: !controller.isLoading.value,
+              replacement: CenteredCircularProgressIndicator(),
+              child: Expanded(
+                child: Obx(() => ListView.builder(
+                  padding: EdgeInsets.only(bottom: 70),
+                  itemCount: controller.newTaskList.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(() => taskDetails(
+                          title:
+                          controller.newTaskList[index].title!,
+                          description: controller
+                              .newTaskList[index].description!,
+                          createdDate: formatDate(
+                              controller
+                                  .newTaskList[index].createdDate!),
+                          status:
+                          controller.newTaskList[index].status!,
+                        ));
+                      },
+                      child: card(
+                        taskType: TaskType.tNew,
+                        taskModel: controller.newTaskList[index],
+                        onTaskStatusUpdated: () {
+                          controller.getTaskCountSummary();
+                          controller.getNewTaskList();
+                        },
+                        onDeleteTask: () {
+                          controller.getTaskCountSummary();
+                          controller.getNewTaskList();
+                        },
+                      ),
+                    );
+                  },
+                )),
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
