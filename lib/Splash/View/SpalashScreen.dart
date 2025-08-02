db0808e7225e048e0e1fe80441db02124395f2f6
Sadid <saadid.2002@gmail.com>
2025-08-02 19:15:55 +0600

    splash screen

--- lib/Splash/View/SpalashScreen.dart
+++ lib/Splash/View/SpalashScreen.dart
@@ -1,41 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import '../../Auth/View/signup_signin/signin.dart';
 import '../../Const/assets.dart';
 import '../../Auth/Controller/controller.dart';
 import '../../Features/appbar_navbar/navbar.dart';
 import '../../Features/Background/Background.dart';
 
-class SpalashScreen extends StatefulWidget {
-  const SpalashScreen({super.key});
+class SplashScreenController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    moveToNextScreen();
+  }
+
+  Future<void> moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    bool isLoggedIn = await AuthController.isUserLoggedIn();
+    if (isLoggedIn) {
+      Get.offNamed(navbar.name);
+    } else {
+      Get.offNamed(signin.name);
+    }
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   static const String name = '/';
 
   @override
-  State<SpalashScreen> createState() => _SpalashScreenState();
-}
-
-class _SpalashScreenState extends State<SpalashScreen> {
-  @override
-  initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _moveToNextScreen();
-    });
-  }
-
-  Future<void> _moveToNextScreen() async {
-    await Future.delayed(Duration(seconds: 2));
-    bool isLoggedIn = await AuthController.isUserLoggedIn();
-    if (isLoggedIn) {
-      Navigator.pushReplacementNamed(context, navbar.name);
-    } else {
-      Navigator.pushReplacementNamed(context, signin.name);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Get.put(SplashScreenController());
     return Scaffold(
       body: background(
         child: Center(child: SvgPicture.asset(AssetPaths.logoSvg)),
