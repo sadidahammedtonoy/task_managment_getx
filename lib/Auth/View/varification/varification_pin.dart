51c4b0e9e32df50a8593ece2fe1cc5b62bdd2d99
Sadid <saadid.2002@gmail.com>
2025-08-02 19:15:30 +0600

    Varification

--- lib/Auth/View/varification/varification_pin.dart
+++ lib/Auth/View/varification/varification_pin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_manager/Auth/View/signup_signin/setpassword.dart';
 import 'package:task_manager/Auth/View/signup_signin/signin.dart';
@@ -12,30 +12,73 @@ import '../../../Features/Background/Background.dart';
 import '../../../Features/Global Widgets/snackbar.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
-class pinVarification extends StatefulWidget {
+class PinVarificationController extends GetxController {
+  final TextEditingController otpTEController = TextEditingController();
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+  var otpisLoading = false.obs;
+
+  void onTapSignInButton() {
+    Get.offAllNamed(signin.name);
+  }
+
+  Future<void> otpVerification() async {
+    otpisLoading.value = true;
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? email = sharedPreferences.getString('email') ?? '';
+    await sharedPreferences.setString('UserOtp', otpTEController.text.trim());
+
+    NetworkResponse response = await networkCaller.getRequest(
+      url: urls.VeriftOtpdUrl(email, otpTEController.text.trim()),
+    );
+    if (response.isSuccess) {
+      VerificationDataModel emailPinVerificationDataModel =
+      VerificationDataModel.fromJson(response.body!);
+
+      String getStatus = emailPinVerificationDataModel.status ?? '';
+      String getData = emailPinVerificationDataModel.data ?? '';
+
+      if (getStatus == 'success') {
+        otpisLoading.value = false;
+        otpTEController.clear();
+        showSnackBarMessage(Get.context!, "$getStatus $getData");
+        showSnackBarMessage(Get.context!, 'Please set your new password');
+        await Future.delayed(Duration(seconds: 1));
+        Get.offAllNamed(Setpassword.name);
+      } else {
+        otpisLoading.value = false;
+        showSnackBarMessage(Get.context!, "$getStatus $getData");
+      }
+    } else {
+      otpisLoading.value = false;
+      showSnackBarMessage(Get.context!, response.errorMessage!);
+    }
+
+    otpisLoading.value = false;
+  }
+
+  @override
+  void onClose() {
+    otpTEController.dispose();
+    super.onClose();
+  }
+}
+
+class pinVarification extends StatelessWidget {
   const pinVarification({super.key});
 
   static const String name = '/emailpinvarification';
 
   @override
-  State<pinVarification> createState() => _pinVarificationState();
-}
-
-class _pinVarificationState extends State<pinVarification> {
-  final TextEditingController _otpTEController = TextEditingController();
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
-  bool _OtpisLoading = false;
-  final FocusNode _focusNode = FocusNode();
-
-  @override
   Widget build(BuildContext context) {
+    final controller = Get.put(PinVarificationController());
     return Scaffold(
       body: background(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
-              key: _key,
+              key: controller.key,
               autovalidateMode: AutovalidateMode.onUserInteraction,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -48,18 +91,16 @@ class _pinVarificationState extends State<pinVarification> {
                   const SizedBox(height: 4),
                   Text(
                     'A 6 digits OTP has been sent to your email address',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.titleSmall?.copyWith(color: Colors.grey),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall
+                        ?.copyWith(color: Colors.grey),
                   ),
                   const SizedBox(height: 24),
                   PinCodeTextField(
                     length: 6,
                     animationType: AnimationType.fade,
                     keyboardType: TextInputType.number,
-                    // autoDismissKeyboard: false,
-                    // autoFocus: true,
-                    // focusNode: _focusNode,
                     pinTheme: PinTheme(
                       shape: PinCodeFieldShape.box,
                       borderRadius: BorderRadius.circular(5),
@@ -71,18 +112,18 @@ class _pinVarificationState extends State<pinVarification> {
                     ),
                     animationDuration: Duration(milliseconds: 300),
                     backgroundColor: Colors.transparent,
-                    controller: _otpTEController,
+                    controller: controller.otpTEController,
                     appContext: context,
                   ),
                   const SizedBox(height: 16),
-                  Visibility(
-                    visible: _OtpisLoading == false,
+                  Obx(() => Visibility(
+                    visible: !controller.otpisLoading.value,
                     replacement: CenteredCircularProgressIndicator(),
                     child: ElevatedButton(
-                      onPressed: () => _OtpVerification(),
+                      onPressed: controller.otpVerification,
                       child: Text('Verify'),
                     ),
-                  ),
+                  )),
                   const SizedBox(height: 32),
                   Center(
                     child: RichText(
@@ -101,7 +142,7 @@ class _pinVarificationState extends State<pinVarification> {
                               fontWeight: FontWeight.w700,
                             ),
                             recognizer: TapGestureRecognizer()
-                              ..onTap = _onTapSignInButton,
+                              ..onTap = controller.onTapSignInButton,
                           ),
                         ],
                       ),
@@ -114,78 +155,5 @@ class _pinVarificationState extends State<pinVarification> {
         ),
       ),
     );
-  }
-
-  void _onTapSubmitButton() {
-    // if (_formKey.currentState!.validate()) {
-    //   // TODO: Sign in with API
-    // }
-    Navigator.pushNamed(context, Setpassword.name);
-  }
-
-  void _onTapSignInButton() {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      signin.name,
-      (predicate) => false,
-    );
-  }
-
-  Future<void> _OtpVerification() async {
-    _OtpisLoading = true;
-    if (mounted) {
-      setState(() {});
-    }
-
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String? Email = sharedPreferences.getString('email') ?? '';
-    await sharedPreferences.setString('UserOtp', _otpTEController.text.trim());
-
-    NetworkResponse response = await networkCaller.getRequest(
-      url: urls.VeriftOtpdUrl(Email, _otpTEController.text.trim()),
-    );
-    if (response.isSuccess) {
-      VerificationDataModel emailPinVerificationDataModel =
-          VerificationDataModel.fromJson(response.body!);
-
-      String getStatus = emailPinVerificationDataModel.status ?? '';
-      String getData = emailPinVerificationDataModel.data ?? '';
-
-      if (getStatus == 'success') {
-        _OtpisLoading = false;
-        if (mounted) {
-          _otpTEController.clear();
-          showSnackBarMessage(context, "$getStatus $getData");
-          showSnackBarMessage(context, 'Please set your new password');
-          await Future.delayed(Duration(seconds: 1));
-          await Navigator.pushNamedAndRemoveUntil(
-            context,
-            Setpassword.name,
-            (predicate) => false,
-          );
-        }
-      } else {
-        if (mounted) {
-          _OtpisLoading = false;
-          showSnackBarMessage(context, "$getStatus $getData");
-        }
-      }
-    } else {
-      if (mounted) {
-        _OtpisLoading = false;
-        showSnackBarMessage(context, response.errorMessage!);
-      }
-    }
-
-    _OtpisLoading = false;
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
-  @override
-  void dispose() {
-    // _otpTEController.dispose();
-    super.dispose();
   }
 }
